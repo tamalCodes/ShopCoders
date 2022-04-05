a1e33ce6ea632d4d8cd1374fec52b61b4cbd1b7f
Tamal Das <gyansujan69@gmail.com>
2022-04-05 14:25:32 +0530

    fixed navbar, bootstrap 5.0

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"
+      ></Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 import styles from "../styles/About.module.css"
+import styleshome from "../styles/Home.module.css"
 
 const About = () => {
     return (
         <>
+            <div className={styleshome.container}>
+                <Navbar />
+            </div>
+
             <div className={styles.main}>
                 <h1>Hello this is the About us page</h1>
             </div>
