6328f22e69b6c0856d38d4d943b9368a451d0bc9
Tamal Das <gyansujan69@gmail.com>
2022-04-08 16:08:14 +0530

    products-listing page layout added.

--- pages/products/shoptshirts.jsx
+++ pages/products/shoptshirts.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import TshirtCard from '../../components/TshirtCard'
+import styles from "../../styles/Shoptshirts.module.css"
+import Navbar from "../../components/Navbar.jsx"
+
 
 const Shoptshirts = () => {
     return (
-        <div>Shoptshirts</div>
+        <>
+            <Navbar />
+            <div className={`container-fluid ${styles.shirtpage_parent}`}>
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+                <TshirtCard />
+            </div>
+
+        </>
     )
 }
 
