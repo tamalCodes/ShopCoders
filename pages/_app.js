6a03da2887e2bce3dcc2f19eaaee76aecc106cef
Tamal Das <gyansujan69@gmail.com>
2022-04-08 22:58:21 +0530

    Added MUI support.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
-import "../styles/globals.css";
+import * as React from "react";
+import PropTypes from "prop-types";
 import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../components/theme";
+import createEmotionCache from "../components/createEmotionCache";
 import Script from "next/script";
 
-function MyApp({ Component, pageProps }) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
-    <>
+    <CacheProvider value={emotionCache}>
       <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
@@ -19,9 +31,18 @@ function MyApp({ Component, pageProps }) {
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"
       ></Script>
-      <Component {...pageProps} />
-    </>
+
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
-export default MyApp;
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
