f62e3294e3b774d61326e863fca1d3ba599f554e
Tamal Das <gyansujan69@gmail.com>
2022-05-05 10:37:44 +0530

    Fixed responsiveness. (#19)

--- pages/detailed-product/[slug].jsx
+++ pages/detailed-product/[slug].jsx
@@ -8,33 +8,39 @@ import Rating from '@mui/material/Rating';
 
 const Detailedproduct = () => {
 
-    const [value, setValue] = React.useState(3);
+
 
     return (
         <>
-            <Navbar />
-            <div className={`container ${styles.detailedproduct_parentmain}`}>
-                <div className={`container-fluid ${styles.detailedproduct_parent}`}>
+            <div className={styles.parent}>
+                <Navbar />
 
-                    <Image src={Shoe} width={500} height={300} alt="Nike" />
-                    <div className={styles.detailedproduct_textdiv}>
-                        <p className={styles.detailedproduct_brandname}>Nike</p>
-                        <h1>Air Max M1</h1>
-                        <Rating name="read-only" value={value} readOnly className={styles.detailedproduct_ratings} />
-                        <p className={styles.detailedproduct_desc}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione eaque dolorum quos fugit, qui perferendis incidunt facere blanditiis ipsa nihil iusto commodi officiis praesentium amet, placeat iste est, itaque nostrum?</p>
+                <div className={`container ${styles.dp_parent}`}>
 
+                    <Image src={Shoe} width={600} height={350} alt="shoe" className={styles.dp_shoeimg} />
 
-                        <hr className={styles.detailedproduct_textdivhr} />
+                    <div className={styles.dp_shoeinfo}>
+                        <span className={styles.dp_companyname}>Nike</span>
+                        <h1> Air Max 270 B1</h1>
 
+                        <div className={styles.dp_rating}>
+                            <Rating name="read-only" value={4} readOnly />
+                        </div>
 
-                        <div className={styles.detailedproduct_btnandprice}>
-                            <h4>$58.00</h4>
+                        <div className={styles.dp_desc_div}>
 
+                            <p className={styles.dp_desc}> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt ut, accusantium minima ipsam id corrupti eum ullam tempora nihil reprehenderit adipisci excepturi repellendus at esse iusto voluptatibus sint nemo. Temporibus.</p>
+                        </div>
 
+                        <hr />
 
-                            <div >
-                                <button className={`btn btn-warning ${styles.detailedproduct_buybtn}`}>Buy now</button>
-                                <FavoriteTwoToneIcon />
+                        <div className={styles.dp_buydiv}>
+                            <div className={styles.dp_buydiv_price}>
+                                <p>$58.00</p>
+                            </div>
+
+                            <div className={styles.dp_buydiv_button}>
+                                <button className={`btn btn-warning ${styles.dp_buybutton}`}>Buy Now</button>
                             </div>
                         </div>
                     </div>
