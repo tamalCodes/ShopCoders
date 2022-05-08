4236e6cfd8e92539589f7e26c35484b4f3ccbc12
Tamal Das <gyansujan69@gmail.com>
2022-05-08 20:52:27 +0530

    Added feature : Slug based product finding, SSR. (#21)

--- pages/detailed-product/[slug].jsx
+++ pages/detailed-product/[slug].jsx
@@ -4,9 +4,11 @@ import Image from 'next/image'
 import Shoe from "../../public/assets/shoe.png"
 import styles from "../../styles/DetailedProduct.module.css"
 import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
+import connectToMongo from '../../middleware/db.js'
+import products from "../../models/ProductSchema"
 import Rating from '@mui/material/Rating';
 
-const Detailedproduct = () => {
+const Detailedproduct = ({ product }) => {
 
 
 
@@ -21,7 +23,7 @@ const Detailedproduct = () => {
 
                     <div className={styles.dp_shoeinfo}>
                         <span className={styles.dp_companyname}>Nike</span>
-                        <h1> Air Max 270 B1</h1>
+                        <h1> {product.name}</h1>
 
                         <div className={styles.dp_rating}>
                             <Rating name="read-only" value={4} readOnly />
@@ -29,14 +31,16 @@ const Detailedproduct = () => {
 
                         <div className={styles.dp_desc_div}>
 
-                            <p className={styles.dp_desc}> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt ut, accusantium minima ipsam id corrupti eum ullam tempora nihil reprehenderit adipisci excepturi repellendus at esse iusto voluptatibus sint nemo. Temporibus.</p>
+                            <p className={styles.dp_desc}> {product.desc}</p>
                         </div>
 
                         <hr />
 
                         <div className={styles.dp_buydiv}>
                             <div className={styles.dp_buydiv_price}>
-                                <p>$58.00</p>
+                                <p>  <strong>Price : </strong>${product.price} </p>
+                                {product.qty === 0 ? <p className={styles.dp_outofstock}>Out of Stock</p> : <p><strong>Items left : </strong>{product.qty}</p>}
+
                             </div>
 
                             <div className={styles.dp_buydiv_button}>
@@ -49,6 +53,22 @@ const Detailedproduct = () => {
 
         </>
     )
+}
+
+
+
+// getting the slug from the url
+// and then i am using that same slug to get a product from the database
+// slugs are unique so we can get the product from the database
+// also then we are passing the product as props to the component
+
+export async function getServerSideProps(context) {
+    connectToMongo();
+    let product = await products.findOne({ slug: context.query.slug });
+
+    return {
+        props: { product: JSON.parse(JSON.stringify(product)) }, // will be passed to the page component as props
+    }
 }
 
 export default Detailedproduct
