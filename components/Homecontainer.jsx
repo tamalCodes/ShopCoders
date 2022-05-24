f5b67e3e96c34e6eb653cf3ca7e4465453c4fa7d
Tamal Das <gyansujan69@gmail.com>
2022-05-24 23:50:30 +0530

    Mobile responsive landing (#32)
    * Fixed Landing for Mobiles.
    
    * Updated Desktop look.

--- components/Homecontainer.jsx
+++ components/Homecontainer.jsx
@@ -1,17 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import sales from "../public/assets/sale.svg";
-import shopwithus from "../public/assets/shopwithus.svg";
-import payment from "../public/assets/payment.svg";
+import whyshopwithus from "../public/assets/whyshopwithus.svg";
 import mobilegh from "../public/assets/MobileGithubImage.jpg"
 import { useUser } from '@auth0/nextjs-auth0';
 import HomeParticles from './HomeParticles';
 import Shoplanding from "../public/assets/Shoplanding.svg"
+import paylesswithus from "../public/assets/paylesswithus.svg";
 
 
 const Homecontainer = () => {
     const { user, error, isLoading } = useUser();
+    const [win, setwin] = useState();
 
     useEffect(() => {
         if (!user) {
@@ -20,6 +21,12 @@ const Homecontainer = () => {
         console.log(user);
         localStorage.setItem('useremail', user.email);
     }, [user]);
+
+    useEffect(() => {
+        // window is accessible here.
+        console.log("window.innerHeight", window.innerHeight);
+        setwin(window.innerHeight);
+    }, []);
 
     return (<>
         <HomeParticles />
@@ -30,9 +37,9 @@ const Homecontainer = () => {
 
         <div className={styles.imagediv2}>
             <div className="row">
-                <div className="col-lg-6"><Image src={Shoplanding} width={800} height={800} alt="wear" className={styles.image1} /></div>
+                <div className="col-lg-6 col-sm-12"><Image src={Shoplanding} width={800} height={800} alt="wear" className={styles.image1} /></div>
 
-                <div className="col-lg-6"> <div className={styles.imagediv2_textdiv}>
+                <div className="col-lg-6 col-sm-12"> <div className={styles.imagediv2_textdiv}>
                     <h3>Wear the Code 🥑</h3>
                     <p>Welcome to ShopCoders, a place for the coders by the coders to get you some premium quality merchendise !!</p>
                     <button className={`btn btn-warning ${styles.explorebtn}`}>Explore</button>
@@ -43,7 +50,72 @@ const Homecontainer = () => {
 
         </div>
 
-        <div className={` ${styles.shopwithusdiv}`}>
+        <hr className={styles.hrbar} />
+
+
+        <div className={styles.imagediv2}>
+
+            <div className={`row ${styles.rowdesktop}`}>
+
+                <div className="col-lg-6 col-sm-12">
+                    <div className={styles.imagediv2_textdiv} style={{ paddingLeft: "10rem" }}>
+                        <h3>Why shop with us ?</h3>
+                        <p> We at ShopCoders offer you official merchandise from all the coding event. These are mostly left out/ not gave-away. We tied up with 50+ official code companies like Github, Digital Ocean to bring you the best of the swags at an affordable price !! </p>
+
+
+                    </div>
+
+                </div>
+
+                <div className="col-lg-6 col-sm-12">
+                    <Image src={whyshopwithus} width={800} height={800} alt="wear" className={styles.image1} />
+                </div>
+
+
+            </div>
+
+            <div className={`row ${styles.rowmobile}`}>
+
+                <div className="col-lg-6 col-sm-12">
+                    <Image src={whyshopwithus} width={800} height={800} alt="wear" className={styles.image1} />
+                </div>
+
+                <div className="col-lg-6 col-sm-12">
+                    <div className={styles.imagediv2_textdiv}>
+                        <h3>Why shop with us ?</h3>
+                        <p>We at ShopCoders offer you official merchandise from all the coding event. These are mostly left out/ not gave-away. We tied up with 50+ official code companies like Github, Digital Ocean to bring you the best of the swags at an affordable price !! </p>
+
+
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+
+
+        <hr className={styles.hrbar} />
+
+
+        <div className={styles.imagediv2} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+            <div className="row">
+                <div className="col-lg-6 col-sm-12"><Image src={paylesswithus} width={750} height={750} alt="wear" className={styles.image1} /></div>
+
+                <div className="col-lg-6 col-sm-12"> <div className={styles.imagediv2_textdiv} style={{ paddingLeft: "10rem" }}>
+                    <h3>Pay less with us !!</h3>
+                    <p>We at ShopCoders have ocassional sales, lottery days, and even we sponser other hackathons where you can get to select a swag as a winner !! <br />
+                        In case you want to gift it to someone, we provide huge discounts from 20 all the way to 80% !!
+                    </p>
+
+
+                </div></div>
+            </div>
+
+
+        </div>
+
+        {/* <div className={` ${styles.shopwithusdiv}`}>
 
             <div className={` ${styles.shopwithus_textdiv}`}>
                 <h2>Why shop with us ?</h2>
@@ -78,7 +150,10 @@ const Homecontainer = () => {
             </div>
 
 
-        </div>
+        </div> */}
+
+
+
 
 
 
