a2bff325134c54be1c67ea1c0f9c634aefa421ee
Tamal <gyansujan69@gmail.com>
2023-01-31 13:02:38 +0530

    feat : New changes to cards

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,9 @@
 import Head from "next/head";
-import { useState } from "react";
 import Footer from "../components/Footer";
 import Homecontainer from "../components/Homecontainer";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
-  const [state, setState] = useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
-
   return (
     <>
       <Head>
