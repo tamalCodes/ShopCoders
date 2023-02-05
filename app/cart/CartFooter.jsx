74fc5adb1c205ca30d96b93d71456cf8582eaea6
Tamal <gyansujan69@gmail.com>
2023-02-06 01:37:34 +0530

    feat : Cart page

---
+++ app/cart/CartFooter.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react'
+
+const CartFooter = () => {
+    return (
+        <div>CartFooter</div>
+    )
+}
+
+export default CartFooter
--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,17 +1,17 @@
 
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 import styles from "../../styles/Cart.module.css"
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-import Cookies from 'js-cookie'
-import { useStore } from '@/global/store'
+import CartFooter from './CartFooter';
 import CartHeader from './CartHeader';
+
 
 const Cart = async () => {
 
+    const cartdetails = await fetch(`${process.env.NEXT_PUBLIC_SHOP_URL}/api/user/viewuserdetails?email=gyansujan69@gmail.com`).then(res => res.json());
 
-
-
+    console.log(cartdetails);
 
 
     return (
@@ -21,7 +21,41 @@ const Cart = async () => {
                 <CartHeader />
 
 
-                {/* {useStore.getState().cartLoading && <p>LOADING</p>} */}
+                <div className={styles.cart_cardparent}>
+                    {cartdetails.user && cartdetails.user.cartproducts.map((product, index) => {
+                        return (<>
+
+                            <Link passHref href={`/products/${product._id}`} className={styles.pb_cardmain} key={index}>
+                                <div className={styles.cart_cartcard}>
+                                    <Image src={product.img} width={380} height={450} alt="cart product" />
+
+                                    <div className={styles.cart_cardtextdiv}>
+                                        <div>
+                                            <p>{product.name}</p>
+                                            <p>{product.desc}</p>
+                                        </div>
+
+
+                                        <div>
+                                            <hr className={styles.cart_cardhr} />
+                                            <p className={styles.cart_cardprice}>${product.price}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+
+
+                        </>)
+                    })}
+                </div>
+                <br />
+                <br />
+
+                <CartFooter />
+
+
+
+
 
             </div>
         </div>
